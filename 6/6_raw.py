b10f0b5ed2f083ec81ce6a43c6446cd7e5e0bf54
Kevin Devine <82415810+kevindevine16@users.noreply.github.com>
2023-12-15 22:03:10 +0000

    Adding day 8

--- 6/6_raw.py
+++ 6/6_raw.py
@@ -14,114 +14,99 @@ pd.set_option('display.max_rows', 10)
 # In[2]:
 
 
-def GetNumbers(string_of_nums):
-    yy = string_of_nums[0].split(' ')
-    b = False
-    while b == False:
-        if '' in yy:
-            yy.remove('')
-        else:
-            b = True
-    return yy
-
-
-def CheckIfMyNumnbersWon(list_of_winning_numbers, list_of_my_numbers):
-    list_of_my_winning_numbers = []
-    for number in list_of_my_numbers:
-        if number in list_of_winning_numbers:
-            list_of_my_winning_numbers.append(int(number))
-        
-    return list_of_my_winning_numbers
+data_path = os.path.join('Data','input.txt')
+data = pd.read_table(data_path)
+data = data.T.reset_index().T.reset_index(drop = True)
+data.rename(columns={0:'inputs'},inplace = True)
+data
 
 
 # In[3]:
 
 
-data_path = os.path.join('Data','input.txt')
-data = pd.read_table(data_path)
-data = data.T.reset_index().T.reset_index(drop = True)
-data.rename(columns={0:'inputs'},inplace = True)
-data['game_number'] = data['inputs'].apply(lambda input: int(input.split(': ')[0].split(' ')[-1]))
-data['winning_Numbers'] = data['inputs'].apply(lambda input: input.split(': ')[1].split('| ')[0].split(', '))
-data['my_Numbers'] = data['inputs'].apply(lambda input: input.split(': ')[1].split('| ')[1].split(', '))
-data['list_of_winning_numbers'] =  data['winning_Numbers'].apply(lambda input: GetNumbers(input))
-data['list_of_my_numbers'] =  data['my_Numbers'].apply(lambda input: GetNumbers(input))
-data['list_of_my_winning_numbers'] = data.apply(lambda df: CheckIfMyNumnbersWon(df['list_of_winning_numbers'], df['list_of_my_numbers']), axis=1)
-data['points_per_card'] = data['list_of_my_winning_numbers'].apply(lambda li: 2**(len(li)-1) if len(li) > 0 else 0)
-data['count_of_my_winning_numbers'] = data['list_of_my_winning_numbers'].apply(lambda li: len(li))
-data['number_of_originals'] = 1
-data['number_of_copies'] = 0
-data
+times = data.iloc[0,0].split('Time: ')[1]
+times =  times.split(' ')
+while '' in times:
+    times.remove('')
+
+times = [ int(time) for time in times ]
+times
 
 
 # In[4]:
 
 
-number_of_copies_dic = {}
-for i in range(len(data)):
-    number_of_copies_dic[i] = 0
-for i in range(len(data)):
-    copy_length = data['count_of_my_winning_numbers'].iloc[i]
-    if (copy_length > 0) :
-        for j in range(i+1,i+copy_length+1):
-            if j <= len(data):
-                number_of_copies_dic[j] += 1 + number_of_copies_dic[i]
-
-    pass
-number_of_copies_dic
+distances = data.iloc[1,0].split('Distance: ')[1]
+distances = distances.split(' ')
+while '' in distances:
+    distances.remove('')
+distances = [ int(distance) for distance in distances ]
+distances
 
 
 # In[5]:
 
 
-count = 0
-for i in range(len(data)):
-    count += number_of_copies_dic[i]
-count+len(data)
+num_of_ways_race = []
+for i in range(len(times)):    
+    num_of_ways = 0
+    for j in range(1,times[i]):
+        if (times[i]-j)*j > distances[i]:
+            num_of_ways+=1
+        pass
+    num_of_ways_race.append(num_of_ways)
+num_of_ways_race
+product = 1
+for number in num_of_ways_race:
+    product *= number
+product
 
 
 # In[6]:
 
 
-def CheckIfMyNumnbersWon(list_of_winning_numbers, list_of_my_numbers):
-    list_of_my_winning_numbers = []
-    for number in list_of_my_numbers:
-        if number in list_of_winning_numbers:
-            list_of_my_winning_numbers.append(int(number))
-        
-    return list_of_my_winning_numbers
-
-
-# In[ ]:
-
-
-
+times = data.iloc[0,0].split('Time: ')[1]
+times =  times.split(' ')
+while '' in times:
+    times.remove('')
+times
+time = ''
+for t in times:
+    time += t
+time = int(time)
 
 
 # In[7]:
 
 
-nums = []
-for yy in data['winning_Numbers'].iloc[0][0].split('  '):
-    print(yy)
-    nums.append(yy.split(' '))
-
-nums
+distances = data.iloc[1,0].split('Distance: ')[1]
+distances =  distances.split(' ')
+while '' in distances:
+    distances.remove('')
+distances
+distance = ''
+for d in distances:
+    distance += d
+distance = int(distance)
 
 
 # In[8]:
 
 
-flat_list = [item for sublist in nums for item in sublist]
-flat_list
+num_of_ways = 0
+for j in range(1,time):
+    if (time-j)*j > distance:
+        num_of_ways+=1
+    pass
+num_of_ways
 
 
-# In[9]:
+# In[10]:
 
 
 import sys
 sys.path.append(os.pardir)
 from src.HelperFunctions import convert_to_py
 
-convert_to_py('4')
+convert_to_py('6')
 
--- 7/7_raw.py
+++ 7/7_raw.py
@@ -14,33 +14,115 @@ pd.set_option('display.max_rows', 10)
 # In[2]:
 
 
+def GetHandType(hand:str)->list:
+    handType = []
+    for character in hand:
+        handType.append(hand.count(character))
+        hand.replace(character,'')
+    handType.sort()
+    return handType
+
+
+# In[3]:
+
+
+def GetHandTypeRank(HandType:list)->int:
+    AllTypes = {tuple([5,5,5,5,5]):7,tuple([1,4,4,4,4]):6,tuple([2,2,3,3,3]):5,tuple([1,1,3,3,3]):4,tuple([1,2,2,2,2]):3,
+                tuple([1,1,1,2,2]):2,tuple([1,1,1,1,1]):1}
+    HandTypeRank = AllTypes[tuple(HandType)]
+    return HandTypeRank
+
+
+# In[4]:
+
+
+def GetIndexRank(hand:str,
+                 index:int)->int:
+    card = hand[index]
+    ranks = {'2':1,'3':2,'4':3,'5':4,'6':5,'7':6,'8':7,'9':8,'T':9,'J':10,'Q':11,'K':12,'A':13}
+    rank = ranks[card]
+    return rank
+
+
+# In[5]:
+
+
 data_path = os.path.join('Data','input.txt')
 data = pd.read_table(data_path)
 data = data.T.reset_index().T.reset_index(drop = True)
 data.rename(columns={0:'inputs'},inplace = True)
-data['hands'] = data['inputs'].apply(lambda imputs:  )
+data['hands'] = data['inputs'].apply(lambda inputs: inputs.split(' ')[0])
+data['bids'] = data['inputs'].apply(lambda inputs: int(inputs.split(' ')[1]))
+data['handTypes'] = data['hands'].apply(lambda hand: GetHandType(hand))
+data['handTypeRanks'] = data['handTypes'].apply(lambda hand: GetHandTypeRank(hand))
+for i in range(5):
+    data['rank_index'+str(i)] = data['hands'].apply(lambda hand: GetIndexRank(hand,i))
+data.sort_values(['handTypeRanks', 'rank_index0','rank_index1','rank_index2','rank_index3','rank_index4'], ascending = True, inplace = True)
+data.reset_index(inplace=True)
+data['rank'] = data.index+1
+data['winnings'] = data['bids']*data['rank']
 data
 
 
-# In[ ]:
+# In[6]:
 
 
+data['winnings'].sum()
 
 
-
-# In[ ]:
-
+# In[7]:
 
 
+def GetIndexRank_2(hand:str,
+                   index:int)->int:
+    card = hand[index]
+    ranks = {'J':1,'2':2,'3':3,'4':4,'5':5,'6':6,'7':7,'8':8,'9':9,'T':10,'Q':11,'K':12,'A':13}
+    rank = ranks[card]
+    return rank
 
 
-# In[ ]:
+# In[8]:
 
 
+def JokerWild(hand:str)->int:
+    HandType = GetHandType(hand)
+    HandTypeRank = int(GetHandTypeRank(HandType))
+    if 'J' in hand:
+        for character in ['2','3','4','5','6','7','8','9','T','Q','K','A']:
+            newHand = hand.replace('J',character)
+            NewHandType = GetHandType(newHand)
+            NewHandTypeRank = int(GetHandTypeRank(NewHandType))
+            if NewHandTypeRank > HandTypeRank:
+                HandTypeRank = NewHandTypeRank
+    return HandTypeRank
 
 
+# In[9]:
 
-# In[3]:
+
+data_path = os.path.join('Data','input.txt')
+data = pd.read_table(data_path)
+data = data.T.reset_index().T.reset_index(drop = True)
+data.rename(columns={0:'inputs'},inplace = True)
+data['hands'] = data['inputs'].apply(lambda inputs: inputs.split(' ')[0])
+data['bids'] = data['inputs'].apply(lambda inputs: int(inputs.split(' ')[1]))
+data['newHandTypeRanks'] = data['hands'].apply(lambda hand: JokerWild(hand))
+for i in range(5):
+    data['rank_index'+str(i)] = data['hands'].apply(lambda hand: GetIndexRank_2(hand,i))
+data.sort_values(['newHandTypeRanks', 'rank_index0','rank_index1','rank_index2','rank_index3','rank_index4'], ascending = True, inplace = True)
+data.reset_index(inplace=True)
+data['rank'] = data.index+1
+data['winnings'] = data['bids']*data['rank']
+data
+
+
+# In[10]:
+
+
+data['winnings'].sum()
+
+
+# In[11]:
 
 
 import sys
